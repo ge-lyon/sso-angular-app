bf96fd0c6296d4ae28414e3f22ebe14c6e75f826
ge-lyon <75308647+ge-lyon@users.noreply.github.com>
2020-12-07 10:16:08 +0100

    Update sso.config.ts

--- src/app/sso.config.ts
+++ src/app/sso.config.ts
@@ -3,18 +3,25 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authConfig: AuthConfig = {
 
   // Url of the Identity Provider
-  issuer: 'https://35.232.234.206/am/ceetal-security-domain/oidc',
+  issuer: 'https://login.microsoftonline.com/ID_ANNUAIRE',
 
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + '/index.html',
 
-  // The SPA's id. The SPA is registerd with this id at the auth-server
-  clientId: '9213c8e8-e89e-4b91-93c8-e8e89e0b916c',
+  // The SPA's id. The SPA is registered with this id at the auth-server. ID_APPLICATION
+  clientId: 'ID_APPLICATION',
+
+  // Resource id : ID_APPLICATION
+  resource: 'ID_APPLICATION',
 
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
-  scope: 'openid',
+  scope: 'openid profile',
 
   // turn off validation that discovery document endpoints start with the issuer url defined above
-  strictDiscoveryDocumentValidation: false
+  strictDiscoveryDocumentValidation: false,
+
+  // Skip the validation of the issuer in the discovery doc. Normally, the discovey document's url starts with the issuer url.
+  skipIssuerCheck: true
+
 }
